a6f12d4d34d18e4fcbe1a587de9cf42ec4d852a1
unknown <smitpotkar20@gmail.com>
2025-07-08 16:23:14 +0530

    Trying to solve APK issue

--- lib/socket.ts
+++ lib/socket.ts
@@ -5,4 +5,7 @@ const socket = io('https://npatgame.onrender.com', {
     forceNew: true,
 });
 
+socket.on('connect', () => console.log('✅ Connected to socket'));
+socket.on('connect_error', (err) => console.log('❌ Socket error:', err));
+
 export default socket;
